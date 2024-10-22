935bb8937d9df902562f63077947ec5d30a5e216
Aaron-Reyes-Rodriguez <aaronreyesrodriguez504@gmail.com>
2024-10-22 16:04:05 -0700

    Swift Project Started (Content View Available)

---
+++ Swift Workout/Swift Workout/Swift_WorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_WorkoutApp.swift
+//  Swift Workout
+//
+//  Created by aaron reyes on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_WorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swift Workout/Swift WorkoutTests/Swift_WorkoutTests.swift
@@ -0,0 +1,16 @@
+//
+//  Swift_WorkoutTests.swift
+//  Swift WorkoutTests
+//
+//  Created by aaron reyes on 10/22/24.
+//
+
+import Testing
+
+struct Swift_WorkoutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
